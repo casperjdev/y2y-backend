3b93adb46e7278eb977f1754a880b216d457fdff
0chamoy0 <1.janek.lipinski@gmail.com>
2025-11-22 18:33:11 +0100

    GET all user's courses endpoint

--- src/extensions/users-permissions/strapi-server.ts
+++ src/extensions/users-permissions/strapi-server.ts
@@ -19,10 +19,43 @@ module.exports = (plugin) => {
       });
   };
 
+  plugin.controllers.user.getCourses = async (ctx) => {
+    if (!ctx.state.user || !ctx.state.user.id) {
+      return (ctx.response.status = 401);
+    }
+    const { id } = ctx.params
+    
+    const courses = await strapi.db
+      .query("api::course.course")
+      .findMany({
+        where: {
+          authors: {
+            id: {
+              $eq: id
+            }
+          },
+          publishedAt: {
+            $notNull: true
+          }
+        }
+      })
+      ctx.body = { courses }
+  }
+
     plugin.routes["content-api"].routes.push({
     method: "POST",
     path: "/user/me",
     handler: "user.updateMe",
+    config: {
+      prefix: "",
+      policies: [],
+    },
+  });
+
+  plugin.routes["content-api"].routes.push({
+    method: "GET",
+    path: "/user/:id/courses",
+    handler: "user.getCourses",
     config: {
       prefix: "",
       policies: [],
